5c6ab7337711e62785b1dffb02363de6432cdfc9
Sujan Sharma <ak@kali.brokenai.ca>
2024-01-14 12:14:53 -0500

    Comments added

--- helloWorld.asm
+++ helloWorld.asm
@@ -1,18 +1,18 @@
+; EVerything after ; is comment
 global _start
 
 
 section text:
 _start:
-	mov eax, 4
-	mov ebx, 1
-	mov ecx, message
-	mov edx, 13
-	int 0x80
+	mov eax, 4 ; sys_write
+	mov ebx, 1 ; stdout
+	mov ecx, message ; pointer to message
+	mov edx, 13 ; length of message 'hello world!\n'
+	int 0x80 ; calling the kernal
 
-	mov eax, 1
-	mov ebx, 60
-	int 0x80
+	mov eax, 1 ; sys_exit
+	mov ebx, 60 ; error code 60
+	int 0x80 ; calling the kernal
 
 section data:
-	message: db "Hello world!", 0x0A
-
+	message: db "Hello world!", 0x0A ; hello world message
